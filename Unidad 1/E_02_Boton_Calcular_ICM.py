58f4682424e8a29fb2f7f714c4dc068003add956
Lex0126 <lorenzo.s.lexiss.18@gmail.com>
2024-02-08 23:56:49 -0600

    Initial commit

---
+++ Unidad 1/E_02_Boton_Calcular_ICM.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5 import uic, QtWidgets
+qtCreatorFile = "E_02_Boton_Calcular_ICM.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+
+        # Área de los Signals
+        self.btn_ICM.clicked.connect(self.icm)
+        self.txt_resultado.setEnabled(False)
+    # Área de los Slots
+    def icm(self):
+        peso = float(self.txt_peso.text())
+        altura = float(self.txt_altura.text())
+        resultado = peso / (altura * altura)
+        resultado_redondeado = round(resultado, 2)
+        cadena = "Tu ICM es de:"+" "+str(resultado_redondeado)
+        self.txt_resultado.setText(cadena)
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
